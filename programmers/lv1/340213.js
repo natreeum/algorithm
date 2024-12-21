45280c7d1a037b72ad649975082d54f2ad138252
natreeum <projectkmscom@gmail.com>
2024-12-21 18:37:56 +0900

    100/100

--- programmers/lv1/340213.js
+++ programmers/lv1/340213.js
@@ -1,15 +1,15 @@
 function solution(video_len, pos, op_start, op_end, commands) {
   function prev() {
-    const newPos = posSec - 10;
-    if (newPos < 0) return 0;
+    let newPos = posSec - 10;
+    if (newPos < 0) newPos = 0;
     if (opStartSec <= newPos && newPos <= opEndSec) {
       return opEndSec;
     }
     return newPos;
   }
   function next() {
-    const newPos = posSec + 10;
-    if (newPos > videoLenSec) return videoLenSec;
+    let newPos = posSec + 10;
+    if (newPos > videoLenSec) newPos = videoLenSec;
     if (opStartSec <= newPos && newPos <= opEndSec) {
       return opEndSec;
     }
@@ -64,7 +64,9 @@ function solution(video_len, pos, op_start, op_end, commands) {
 const case1 = ["34:33", "13:00", "00:55", "02:55", ["next", "prev"]];
 const case2 = ["10:55", "00:05", "00:15", "06:55", ["prev", "next", "next"]];
 const case3 = ["07:22", "04:05", "00:15", "04:07", ["next"]];
+const case4 = ["10:00", "00:03", "00:00", "00:05", ["prev", "next"]];
 
 console.log(solution(case1[0], case1[1], case1[2], case1[3], case1[4]));
 console.log(solution(case2[0], case2[1], case2[2], case2[3], case2[4]));
 console.log(solution(case3[0], case3[1], case3[2], case3[3], case3[4]));
+console.log(solution(case4[0], case4[1], case4[2], case4[3], case4[4]));
