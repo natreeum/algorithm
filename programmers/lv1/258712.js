337617276c30533301164577592f8932ba212180
natreeum <projectkmscom@gmail.com>
2024-12-21 17:52:36 +0900

    100/100

--- programmers/lv1/258712.js
+++ programmers/lv1/258712.js
@@ -1,51 +1,41 @@
 function solution(friends, gifts) {
-  // 1. 초기화
-  const giftInd = {};
-  const expectedGift = {};
-  for (let f of friends) {
-    expectedGift[f] = 0;
-    giftInd[f] = 0;
+  const giftMap = Array.from({ length: friends.length }, () =>
+    Array(friends.length).fill(0)
+  );
+  const giftIdx = Array.from({ length: friends.length }, () =>
+    Array(2).fill(0)
+  );
+
+  // parse Input
+  for (let g of gifts) {
+    const [giver, receiver] = g.split(" ");
+
+    const giverIdx = friends.indexOf(giver);
+    const receiverIdx = friends.indexOf(receiver);
+    giftMap[giverIdx][receiverIdx]++;
+
+    giftIdx[giverIdx][0]++;
+    giftIdx[receiverIdx][1]++;
   }
 
-  // 2. 선물지수 계산
-  for (let f of friends) {
-    const gave = gifts.filter((e) => e.split(" ")[0] == f).length;
-    const received = gifts.filter((e) => e.split(" ")[1] == f).length;
-
-    giftInd[f] = gave - received;
-  }
-
-  // 3. 받을 선물 계산
-  for (let f1 of friends) {
-    for (let f2 of friends) {
-      if (f1 == f2) continue;
-      // 3-1. 주고받은 선물이 있는지 확인
-      const f1Count = gifts.filter(
-        (e) => e.split(" ")[0] == f1 && e.split(" ")[1] == f2
-      ).length;
-      const f2Count = gifts.filter(
-        (e) => e.split(" ")[0] == f2 && e.split(" ")[1] == f1
-      ).length;
-
-      // 3-1-1. 없거나 같다면 선물지수 고려
-      if (f1Count - f2Count == 0) {
-        // 3-1-1-1. 선물지수가 큰 사람이 받음
-        if (giftInd[f1] < giftInd[f2]) expectedGift[f2]++;
-        if (giftInd[f2] < giftInd[f1]) expectedGift[f1]++;
-        // 3-1-1-2. 선물지수도 같다면 아무도 안받음
-        if (giftInd[f2] == giftInd[f1]) continue;
-      }
-
-      // 3-1-2. 더 많은 선물을 준 사람이 받음
-      else if (f1Count < f2Count) {
-        expectedGift[f2]++;
-      } else if (f2Count < f1Count) {
-        expectedGift[f1]++;
+  // compare
+  let answer = -1;
+  for (let i = 0; i < friends.length; i++) {
+    let expectedGiftCount = 0;
+    for (let j = 0; j < friends.length; j++) {
+      if (i == j) continue;
+      const iGave = giftMap[i][j];
+      const jGave = giftMap[j][i];
+      if (iGave > jGave) expectedGiftCount++;
+      else if (iGave == jGave) {
+        const iGiftIdx = giftIdx[i][0] - giftIdx[i][1];
+        const jGiftIdx = giftIdx[j][0] - giftIdx[j][1];
+        if (iGiftIdx > jGiftIdx) expectedGiftCount++;
       }
     }
+    if (answer < expectedGiftCount) answer = expectedGiftCount;
   }
-
-  return Math.max(...Object.values(expectedGift)) / 2;
+  return answer;
 }
 
 const friends_C1 = ["muzi", "ryan", "frodo", "neo"];
